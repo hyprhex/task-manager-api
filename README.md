60c51d76ab9fbe92a49680a7d4719d96325104fa
AIpill <swapapis@gmail.com>
2023-11-18 07:32:55 +0100

    Explain API Endpint, Task parameter and write task parameter with sample tasks

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Creating powerful and efficient REST API using Go + Gin framework
 
 [Install Go](https://go.dev/doc/install)
 
+## API Endpoint 
+
+- `GET /tasks`: Get a list of all tasks.
+- `GET /tasks/id`: Get the details of a specific task.
+- `PUT /tasks/id`: Update a specific task. 
+- `DELETE /tasks/id`: Delete a specific task.
+- `POST /tasks`: Create a new task.
+
+## Task Parameter
+
+Every Task has: `Title | Description | DueDate | Status`
--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+	"github.com/gin-gonic/gin"
+)
+
+// Tasks parameter 
+type Task struct {
+	ID						string `json:"id"` 
+	Title					string `json:"title"`
+	Description   string `json:"description"`
+	DueDate				time.Time `json:"due_date"`
+	Status				string `json:"status"`
+}
+
+// Simple tasks 
+var tasks = []Task {
+	{ID: "1", Title: "Task 1", Description: "First Task", DueDate: time.Now(), Status: "Pending"},
+	{ID: "2", Title: "Task 2", Description: "Second Task", DueDate: time.Now().AddDate(0, 0, 1), Status: "Pending"},
+	{ID: "3", Title: "Task 3", Description: "Third Task", DueDate: time.Now().AddDate(0, 0, 2), Status: "Pending"},
+}
 
 func main() {
 	router := gin.Default()
